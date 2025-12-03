8fc8d79692aa40beb5bf40c5e6b4998f065fb5c6
PiotrLeb <piotr.lebioda14@gmail.com>
2025-12-03 23:06:29 +0100

    Commit

---
+++ app/src/main/java/com/plcoding/runique/RuniqueApp.kt
@@ -0,0 +1,40 @@
+package com.plcoding.runique
+
+import android.app.Application
+import com.plcoding.auth.data.di.authDataModule
+import com.plcoding.auth.presentation.di.authViewModelModule
+import com.plcoding.core.data.di.coreDataModule
+import com.plcoding.run.location.di.locationModule
+import com.plcoding.run.presentation.di.runPresentationModule
+import com.plcoding.runique.di.appModule
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class RuniqueApp: Application() {
+
+    val applicationScope = CoroutineScope(SupervisorJob())
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+        startKoin {
+            androidLogger()
+            androidContext(this@RuniqueApp)
+            modules(
+                authDataModule,
+                authViewModelModule,
+                appModule,
+                coreDataModule,
+                runPresentationModule,
+                locationModule
+            )
+        }
+    }
+}
